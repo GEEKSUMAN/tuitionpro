e4a513a47f6acf8235f2e8bd965f3d99ce00f570
SUMAN MAJI <iamsuman1808@gmail.com>
2020-05-24 14:39:26 +0530

    modified:   application/models/Login_model.php

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -10,17 +10,19 @@ class Login_model extends CI_Model
    foreach($query->result() as $row)
    {
      $store_password = $this->encryption->decrypt($row->password);
-     if($password == $store_password)
+     if($password == $store_password && $row->status==1)
      {
       $this->session->set_userdata('user_id', $row->id);
       $this->session->set_userdata('user_email', $row->email);
       $this->session->set_userdata('full_name', $row->full_name);
       $this->session->set_userdata('registration_type', $row->registration_type);
       $this->session->set_userdata('status', $row->status);
+     } elseif ($row->status !=1) {
+       return 'Your account has been disabled. Contact web admin.';
      }
      else
      {
-      return 'Wrong Password';
+      return 'Wrong Password ';
      }
    }
   }
